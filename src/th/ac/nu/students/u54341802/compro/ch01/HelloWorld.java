759f33b158d7eb3faff2e8e608bd62d9dfc1058b
Onuma  Naknoy <lovelove_on@hotmail.com>
2012-06-19 17:44:28 +0700

    สร้าง HelloWorld.java

--- src/th/ac/nu/students/u54341802/compro/ch01/HelloWorld.java
+++ src/th/ac/nu/students/u54341802/compro/ch01/HelloWorld.java
@@ -2,17 +2,19 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+package th.ac.nu.students.u54341802.compro.ch01;
 
 /**
  *
  * @author admin
  */
-public class Compro1802 {
+public class HelloWorld {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello, World");
     }
 }
